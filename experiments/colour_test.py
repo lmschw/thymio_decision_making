a39ed56e4db1e631f570d22b866eeac2c7d8cc23
Lilly <lillyschwarzenbach@gmail.com>
2026-07-09 10:37:45 +0200

    add colour test

---
+++ experiments/colour_test.py
@@ -0,0 +1,52 @@
+import asyncio
+
+class ColourTestExperiment:
+
+    def __init__(self, robot, config=None, logger=None):
+        self.robot = robot
+        self.config = config or {}
+        self.logger = logger
+
+        self.running = True
+        self.paused = False
+
+    async def run(self):
+        while self.running:
+
+            if self.paused:
+                await self.robot.stop()
+                await asyncio.sleep(0.1)
+                continue
+
+            await self.robot.drive(50, 50)
+
+            reflected = await self.robot.proximity_ground_reflected()
+
+            if self.logger:
+                self.logger.log(
+                    state={"reflected_0": reflected[0], "reflected_1": reflected[1]},
+                    command={}
+                )
+
+            await asyncio.sleep(1)
+
+            await self.robot.drive(-50, -50)
+
+            if self.logger:
+                self.logger.log(
+                    state={"reflected_0": reflected[0], "reflected_1": reflected[1]},
+                    command={}
+                )
+
+            await asyncio.sleep(1)
+
+        await self.robot.drive(0, 0)
+
+    async def pause(self):
+        self.paused = True
+
+    async def resume(self):
+        self.paused = False
+
+    async def stop(self):
+        self.running = False
