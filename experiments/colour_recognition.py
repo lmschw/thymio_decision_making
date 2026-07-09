a55e679da0b41432eb1500196ce5b979652e5257
Lilly <lillyschwarzenbach@gmail.com>
2026-07-09 13:47:50 +0200

    add colour recognition

--- experiments/colour_recognition.py
+++ experiments/colour_recognition.py
@@ -1,9 +1,10 @@
 import asyncio
 import math
 
+from behaviours.colour_recognition import GroundColourSensor, GroundColour
 from behaviours.obstacle_avoidance import ObstacleAvoidance
 
-class ObstacleAvoidanceExperiment:
+class ColourRecognitionExperiment:
 
     def __init__(self, robot, config=None, logger=None):
         self.robot = robot
@@ -20,6 +21,7 @@ class ObstacleAvoidanceExperiment:
 
         self.turning_left = 0
 
+        self.ground_sensor = GroundColourSensor()
         self.obstacle_avoidance = ObstacleAvoidance(wheel_velocity=self.wheel_velocity,
                                                     delta=self.delta,
                                                     turn_steps=self.turn_steps)
@@ -45,6 +47,21 @@ class ObstacleAvoidanceExperiment:
                 continue
 
             prox = await self.robot.proximity_horizontal()
+            reflected = await self.robot.proximity_ground_reflected()
+
+            colour = self.ground_sensor.sense_ground_colour(self.robot)
+
+            match colour:
+                case GroundColour.BLACK:
+                    await self.robot.top_led(0, 0, 32)
+                case GroundColour.GREY:
+                    await self.robot.top_led(0, 32, 0)
+                case GroundColour.WHITE:
+                    await self.robot.top_led(32, 0, 0)
+                case GroundColour.GROUND_NEUTRAL:
+                    await self.robot.top_led(32, 32, 0)
+                case GroundColour.GROUND_NEUTRAL:
+                    await self.robot.top_led(0, 32, 32)
 
             left, right = self.obstacle_avoidance.step_motion(prox)
 
@@ -52,7 +69,11 @@ class ObstacleAvoidanceExperiment:
 
             if self.logger:
                 self.logger.log(
-                    state={"proximity": prox},
+                    state={"proximity": prox, 
+                           "reflected_0": reflected[0], 
+                           "reflected_1": reflected[1],
+                           "reflected_avg": (reflected[0] + reflected[1])/2,
+                           "colour": colour},
                     command={
                         "left_motor": left,
                         "right_motor": right,
