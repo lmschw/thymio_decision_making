549e33d4c75d561145f3dbcf797ba4dbf6dc2341
Lilly <lillyschwarzenbach@gmail.com>
2026-08-25 16:56:19 +0200

    revert changes

--- behaviours/base_behaviours/obstacle_avoidance.py
+++ behaviours/base_behaviours/obstacle_avoidance.py
@@ -5,7 +5,7 @@ class ObstacleAvoidance:
     def __init__(
         self,
         wheel_velocity=100,
-        delta=1400,
+        delta=800,
     ):
         self.wheel_velocity = wheel_velocity
         self.delta = delta
